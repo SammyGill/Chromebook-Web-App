baacd76dbe23a27cc3b498807f4cab13f2312311
Sammy Gill <sammygill98@gmail.com>
2017-07-05 17:46:47 -0700

    Fixed all links for the search and overview pages

--- search.php
+++ search.php
@@ -64,7 +64,7 @@
       <div class="col-sm-3 col-md-2 sidebar">
 
         <ul class="nav nav-sidebar">
-          <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+          <li class="active"><a href="overview.html">Overview <span class="sr-only">(current)</span></a></li>
           <li><a href="analytics.html">Analytics</a></li>
           <li><a href="export.html">Export</a></li>
         </ul>
