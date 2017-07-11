e3a6fb504d251b9b7744b6d710dd5b8c8c508d80
Sammy Gill <sammygill98@gmail.com>
2017-07-11 01:07:43 -0700

    tested with other files

--- ChromebookFileReader/readFile.c
+++ ChromebookFileReader/readFile.c
@@ -3,22 +3,32 @@
 #include<string.h>
 #include<dirent.h>
 
-void compareData(char *room, char *asset, char **unfoundChromebooks, int *index, FILE *stream) {
-  char searchString[BUFSIZ] = {0};
+#define MAX_STR 100
+#define ASSET_LEN 4
+
+void compareData(char *school, char *room, char *asset, char *status, char *assignment, char **unfoundChromebooks, int *index, FILE *stream) {
+  char searchString[MAX_STR] = {0};
   FILE *fp = fopen("Chromebook Mass Export.txt", "r");
   if(fp == NULL) {
     printf("COULD NOT OPEN OTHER FILE\n");
     return;
   }
-  while(fgets(searchString, BUFSIZ, fp)) {
+  while(fgets(searchString, MAX_STR, fp)) {
     char *orgUnit = strtok(searchString, ",");
     char *roomNote = strtok(NULL, ",");
     char *assetNote = strtok(NULL, ",");
     char *serialNumber = strtok(NULL, ",");
     char *model = strtok(NULL, ",");
+    char modelArray[MAX_STR];
+    if(model != NULL) {
+      strcpy(modelArray, model);
+      char *newLinePtr = strchr(modelArray, '\n');
+      *newLinePtr = '\0';
+    }
 
-    if(strncmp(asset, assetNote, 4) == 0) {
-      fprintf(stream, "%s | %s | %s", assetNote, serialNumber, model);
+
+    if(strncmp(asset, assetNote, ASSET_LEN) == 0) {
+      fprintf(stream, "%s | %s | %s | %s | %s| %s | %s", school, room, assetNote, serialNumber, modelArray, status, assignment);
       fclose(fp);
       return;
     }
@@ -31,7 +41,7 @@ void compareData(char *room, char *asset, char **unfoundChromebooks, int *index,
 
 int main() {
   int unfoundIndex = 0;
-  char *unfoundChromebooks[BUFSIZ] = {0};
+  char *unfoundChromebooks[MAX_STR] = {0};
   DIR *path = opendir("Data");
   struct dirent *dp;
   FILE *writeFile = fopen("output.txt", "w");
@@ -47,12 +57,12 @@ int main() {
     }
 
 
-    char string[BUFSIZ] = {0};
+    char string[MAX_STR] = {0};
 
 
 
-    char dirName[50] = "Data/";
-    char fullPath[50] = {0};
+    char dirName[MAX_STR] = "Data/";
+    char fullPath[MAX_STR] = {0};
     strcpy(fullPath, dirName);
     strcat(fullPath, dp->d_name);
     FILE *fp = fopen(fullPath, "r");
@@ -63,24 +73,27 @@ int main() {
       printf("SUCCESSFULLY OPENED FILE %s\n", dp->d_name);
     }
 
-    while(fgets(string, BUFSIZ, fp)) {
-      char *room = strtok(string, ",");
+    while(fgets(string, MAX_STR, fp)) {
+      char *school = strtok(string, ",");
+      char *room = strtok(NULL, ",");
       char *asset = strtok(NULL, ",");
-      compareData(room, asset, &unfoundChromebooks[unfoundIndex], &unfoundIndex, writeFile);
+      char *status = strtok(NULL, ",");
+      char *assignment = strtok(NULL, ",");
+      compareData(school, room, asset, status, assignment, &unfoundChromebooks[unfoundIndex], &unfoundIndex, writeFile);
     }
+    printf("FINISHED READING FILE %s\n", dp->d_name);
     fclose(fp);
-
   }
 
   for(int i = 0; i < unfoundIndex; i++) {
-    fprintf(writeFile, "COULD NOT FIND %s", unfoundChromebooks[i]);
+    fprintf(writeFile, "COULD NOT FIND %s\r\n", unfoundChromebooks[i]);
   }
 
   for(int i = 0; i < unfoundIndex; i++) {
     free(unfoundChromebooks[i]);
   }
+
   fclose(writeFile);
   closedir(path);
   return EXIT_SUCCESS;
-
 }
